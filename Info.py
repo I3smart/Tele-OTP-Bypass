d9aad0678e2b8437b5d02dc1b58bf0a8297bb10b
Yumiko-Bots <131843574+Yumiko-Bots@users.noreply.github.com>
2023-12-03 01:25:38 +0530

    Update Info.py

--- Info.py
+++ Info.py
@@ -1,12 +1,12 @@
 #Twilio
 
-account_sid = ''
-auth_token = ''
+account_sid = 'ACd9dd55ec4ed9f4d858c45cc3b551f53f'
+auth_token = 'e0ca978c7ed0d9488a7842204dd250fd'
 twilionumber = ''
 twiliosmsnumber = ''
 
 #Telegram Bot Token
-API_TOKEN = "" 
+API_TOKEN = "6667196156:AAEAlmKfW6dYp79Tn44WBXDZBG8V-UXp0qo" 
 
 #Host URL
 callurl = 'https://03f3-196-191-60-243.ngrok.io' 
